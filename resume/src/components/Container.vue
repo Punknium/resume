4d4a31575cddbbfa008f2d40547c1df667fd8aee
Ryder Smith <rsmith@kbsi.com>
2019-11-20 16:35:20 -0600

    Started on header

---
+++ resume/src/components/Container.vue
@@ -0,0 +1,22 @@
+<!--
+ *
+ * Created by Ryder Smith on 11/20/2019.
+ *
+ * Copyright (c) 2019, Knowledge Based Systems, Incorporated,
+ * College Station, Texas USA.  All rights reserved.
+ *
+-->
+<template>
+  <div class="container mx-auto min-h-screen p-4 bg-gray-200">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Container',
+}
+</script>
+
+<style scoped>
+</style>
---
+++ resume/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="w-full text-center">
+    <p class="text-3xl font-semibold">First Last</p>
+    <p>Address: 123 Main St, Somewhere, TX | (555) 555-5555 | email@mail.com</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style>
+
+</style>
